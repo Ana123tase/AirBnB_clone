a3cff796b3bcee321032cce34bff9ce08142fcf8
Ana123tase <anastasedukunda@gmai.com>
2024-02-11 01:43:58 -0500

    city cls and it's definition

---
+++ models/place.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines the Place class."""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Represent a place.
+
+    Attributes:
+        city_id: City's ID
+        user_id: User's ID
+        name: place's name(s)
+        description: Brief overview opf the city
+        max_guest: Max_num of guests that a place can hold at a time
+        price_by_night: Total price per night          
+        number_bathrooms: The present numer of bathrooms in a pave
+        latitude (float): latitude
+        longitude: longitudes
+        amenity_ids: New identities
+    """
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
